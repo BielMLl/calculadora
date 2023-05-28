3b13b7179e16485c2df95b741f8fe36fc4bbf207
BielMLl <116098089+BielMLl@users.noreply.github.com>
2023-05-28 17:39:38 +0200

    Update Estructures_alternatives_II.java

--- Estructures_alternatives_II.java
+++ Estructures_alternatives_II.java
@@ -16,27 +16,7 @@ public class Estructures_alternatives_II {
     
     Scanner teclat = new Scanner(System.in);
   
-        
-    // ex1 - Escriu un programa que llegeixi tres números enters i ens digui quin és el valor més gran.
-        
-    /*    System.out.println("Anomena el primer numero:");
-        int numero1 = teclat.nextInt();
-        System.out.println("Anomena el segon numero:");
-        int numero2 = teclat.nextInt();
-        System.out.println("Anomena el tercer numero:");
-        int numero3 = teclat.nextInt();
-        
-        if (numero1 > numero2 && numero1 > numero3){
-            System.out.println("El numero mes gran es: " + numero1);
-        } else if (numero2 > numero1 && numero2 > numero3){
-            System.out.println("el numero mes gran es: " + numero2);
-        } else if (numero3 > numero1 && numero3 > numero2);
-                   System.out.println("El numero mes gran es: " + numero3);
-     */
-    
-    // ex2 - Escriu un programa que simuli una calculadora simple. El programa llegeix dos enters i un caràcter. Si el caràcter és un + s’imprimeix la suma, si és un – la resta, si és un * el producte, si és un / la divisió entera i si és un %, el mòdul.
-    
-    /*               
+             
         char operador;
         Double num1, num2, resultat;
 
@@ -78,34 +58,6 @@ public class Estructures_alternatives_II {
         System.out.println("Operador Incorrecte!");
         break;
         }
-    */
-        
-        
-    // ex3 - Escriu un programa en el qual a partir d’una data inicial llegida per teclat amb el format dia, mes, any (guardats amb variables diferents), ens digui la data del dia següent.
-    
-    /*    System.out.println("Dia actual ??");
-        int Dia = teclat.nextInt();
-        
-        System.out.println("Mes actual ??");
-        int Mes = teclat.nextInt();
-        
-        System.out.println("Any actual ??");
-        int Any = teclat.nextInt();
-        */
-    
-    // ex4 - Els treballadors d’una fàbrica treballen en dos torns: diürn i nocturn. Es vol calcular el salari d’un dia d’acord amb els següents punts:
-    
-    int dia, mes, any, diaActual;
-    
-        System.out.println("introduieix el dia");
-        dia = teclat.nextInt();
-        System.out.println("Introdueix el mes:");
-        mes = teclat.nextInt();
-        System.out.println("introdueix l'any: ");
-        any = teclat.nextInt();
-        
-        diaActual = (dia) + (mes) + (any);
-        System.out.println("Dia actual:" + diaActual);
     
     }
 }
